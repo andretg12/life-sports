5a3f7d2d5779c986dfb8e1b782aba077d98a2121
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-18 14:17:09 -0400

    Added to schemas

--- models/coach.model.js
+++ models/coach.model.js
@@ -3,36 +3,65 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const coachSchema = new Schema({
+    username: {
+        type:String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/, "Must contain one uppercase letter, one number, one special character, and be at least 6 characters long"],
+        minlength: 6,
+        trim: true
+    },
     firstName: {
         type: String,
         required: true,
+        trim: true
     },
     lastName: {
         type: String,
         required: true,
+        trim: true
     },
     street: {
         type: String,
+        trim: true,
+        required: true
     },
     state: {
         type: String,
+        trim: true,
+        required: true
+    },
+    academy:{type: String, required: true},
+    country: {
+        type: String,
+        required: true,
+        default: "US",
+        trim: true
     },
     phone: {
         type: Number,
-        required: true
+        required: true,
+        trim: true
     },
     locations: {
         type: [String],
-        required: true
-    },
-    recommendations: {
-        type: [String]
+        required: true,
+        trim: true
     },
     picture: {
-        type: String
+        type: String,
+        trim: true,
+        default: "https://www.pinclipart.com/picdir/middle/322-3222849_north-carolina-tar-heels-duke-blue-devils.png"
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        trim: true
     }
 })
---
+++ models/students.model.js
@@ -0,0 +1,258 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  active: {type: Boolean, required: true, default: false},
+  password: {
+    type: String,
+    required: true,
+    match: [/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/, "Must contain one uppercase letter, one number, one special character, and be at least 6 characters long"],
+    minlength: 6,
+    trim: true
+},
+firstName: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength: 2
+},
+lastName: {
+  type: String,
+  required: true,
+  trim: true,
+  minlength: 2
+},
+
+street: {
+  type: String,
+  trim: true,
+  required: true,
+  minlength: 2
+},
+state: {
+  type: String,
+  trim: true,
+  required: true,
+  minlength: 2
+},
+academy:{type: String, required: true, minlength: 2},
+country: {
+  type: String,
+  required: true,
+  default: "US",
+  trim: true,
+  minlength: 2
+},
+schoolName: {
+  type: String,
+  trim: true,
+  required: true,
+  minlength: 2
+},
+schoolId: {
+  type: String,
+  required: true,
+  
+},
+picture: {
+  type: String,
+  trim: true,
+  default: "https://www.pinclipart.com/picdir/middle/322-3222849_north-carolina-tar-heels-duke-blue-devils.png"
+},
+basketballStats: {
+  type: {
+    position: {
+      type: String,
+      required: true,
+      defalut: "N/A"
+    },
+    minutesPlayed : {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    points: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    assists: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    blocks: {
+      types: Number,
+      default: 0,
+      required: true,
+    },
+    steals: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    fga: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    fgm: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    freeTA: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    freeTM: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    rebounds: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    gamesPlayed: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    tpm: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    tpa: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    turnovers: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    fouls: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+    
+    
+  }
+},
+soccerStats: {
+  type: {
+    position: {
+      type: String,
+      required: true,
+      default: "N/A"
+    },
+    goals: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    matchesPlayed: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    playingTime: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    assists: {
+      type: Number,
+      required: true,
+       default: 0
+    },
+    yellowCards: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    redCards: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    saves: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    goalsAgainst: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    fouls: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    passAttempt: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    passMade: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    goalAttempt: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    onTarget: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    milesRun: {
+      type: Number,
+      required: true,
+      default: 0
+    }
+  }
+},
+academicStats: {
+  type: {
+    booksRead: [String],
+    gpa: {
+      type: String,
+      required: true,
+      trim: true,
+      default: "N/A"
+    },
+    readingLvl: {
+      type: String,
+      trim: true,
+      required: true,
+      default: "N/A"
+    }
+  }
+}
+}, {
+  timestamps: true,
+}); 
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
