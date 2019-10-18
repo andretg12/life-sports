a28c2fe7fd48867939062be07f0031c3630fa295
Skyler Stevens <sabrinastevens@Administrators-MacBook-Pro.local>
2019-10-18 15:07:57 -0400

    Add academy Schema

--- models/academy.model.js
+++ models/academy.model.js
@@ -3,5 +3,96 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const academySchema = new Schema({
-
+    name: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    location: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    street: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    city: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    state: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    zipcode: {
+        type: Number,
+        required: true,
+        minlength: 2
+    },
+    country: {
+        type: String,
+        required: true,
+        minlength: 2,
+        default: "US"
+    },
+    phoneNum: {
+        type: Number,
+        required: true,
+        minlength: 2
+    },
+    coach: {
+        type: String,
+        required: true,
+        minlength: 2
+    },
+    resources: {
+        readingList: [{
+                title: {
+                    type: String,
+                    required: true
+                },
+                author: {
+                    type: String,
+                    required: true
+                },
+                URL: {
+                    type: String
+                }
+            }],
+        academicArticles: [{
+            title: {
+                type: String,
+                required: true
+            },
+            URL: {
+                type: String,
+                required: true
+            }
+        }],
+        drillVideos: {
+            title: {
+                type: String,
+                required: true
+            },
+            URL: {
+                type: String,
+                required: true
+            }
+        },
+        sportsArticles: {
+            title: {
+                type: String,
+                required: true
+            },
+            URL: {
+                type: String,
+                required: true
+            }
+        }
+    },
+    
 })
