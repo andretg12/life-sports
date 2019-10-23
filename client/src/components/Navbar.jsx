250e970667e91d9cdabde0adbf0d46c6e2836c79
Andre Torrealba <andre.torrealba@icloud.com>
2019-10-23 15:28:38 -0400

    fixed coach schema

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,54 +3,56 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
 	return (
-		<nav
-			className="navbar navbar-light navbar-expand-lg"
-			style={{ backgroundColor: "#FBCA1E" }}
-		>
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarSupportedContent"
-				aria-controls="navbarSupportedContent"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
+		<div>
+			<nav
+				className="navbar navbar-light navbar-expand-lg"
+				style={{ backgroundColor: "#FBCA1E" }}
 			>
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<Link to="/" className="navbar-brand">
-					LIFESPORTS
-				</Link>
-				<ul className="navbar-nav mr-auto">
-					<li className="navbar-item">
-						<Link to="/" className="nav-link">
-							STUDENTS
-						</Link>
-					</li>
-					<li className="navbar-item">
-						<Link to="/resources" className="nav-link">
-							RESOURCES
-						</Link>
-					</li>
-					<li className="navbar-item">
-						<Link to="/student/signup" className="nav-link">
-							ADD STUDENT
-						</Link>
-					</li>
-					<li className="navbar-item">
-						<Link to="/coach/signup" className="nav-link">
-							ADD COACH
-						</Link>
-					</li>
-					<li>
-						<Link to="/exercise" className="nav-link">
-							EXERCISES
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<Link to="/" className="navbar-brand">
+						LIFESPORTS
+					</Link>
+					<ul className="navbar-nav mr-auto">
+						<li className="navbar-item">
+							<Link to="/" className="nav-link">
+								STUDENTS
+							</Link>
+						</li>
+						<li className="navbar-item">
+							<Link to="/resources" className="nav-link">
+								RESOURCES
+							</Link>
+						</li>
+						<li className="navbar-item">
+							<Link to="/student/signup" className="nav-link">
+								ADD STUDENT
+							</Link>
+						</li>
+						<li className="navbar-item">
+							<Link to="/coach/signup" className="nav-link">
+								ADD COACH
+							</Link>
+						</li>
+						<li>
+							<Link to="/exercise" className="nav-link">
+								EXERCISES
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	);
 }
 export default Navbar;
